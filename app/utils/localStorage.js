7997e7ddd4906074fe16d9c0832fda1884e8e5d3
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-07-11 18:51:47 +0800

    Added localForage as wrapper for indexDB

---
+++ app/utils/localStorage.js
@@ -0,0 +1,21 @@
+import localForage from 'localForage'
+
+/**
+ * Please wrap this instance to a try/catch block
+ */
+
+export async function setItem (...args) {
+  return await localForage.setItem(...args) // eslint-disable-line
+}
+
+export async function getItem (key) {
+  return await localForage.getItem(key) // eslint-disable-line
+}
+
+export async function removeItem (key) {
+  return await localForage.removeItem(key) // eslint-disable-line
+}
+
+export async function clear () {
+  return await localForage.clear() // eslint-disable-line
+}
