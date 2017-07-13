9a46725040d4d5ded74ba8039c137e8562d9aa5b
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2017-07-13 14:07:26 +0800

    initial navcategory component

---
+++ app/components/NavCategories/messages.js
@@ -0,0 +1,13 @@
+/*
+ * NavCategories Messages
+ *
+ * This contains all the text for the NavCategories component.
+ */
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  header: {
+    id: 'app.components.NavCategories.header',
+    defaultMessage: 'This is the NavCategories component !'
+  }
+})
