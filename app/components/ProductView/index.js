d6acf358503f08ca258fd208542ec980928e7a9d
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-07-28 15:00:28 +0800

    Remove calculationPromo on product and use the helper instead

--- app/components/ProductView/index.js
+++ app/components/ProductView/index.js
@@ -32,53 +32,7 @@ import ProductImage from 'images/test-images/samplebag.png'
 import EmptyImage from 'images/broken-image.jpg'
 import ParagraphImage from 'images/test-images/short-paragraph.png'
 
-/**
- * Main calculation for getting the total price
- * @param {*} price
- * @param {*} discount
- */
-const getTotalPrice = (price = 0, discount = 0) => {
-  return price - (price * (discount / 100))
-}
-
-/**
- * Currying for instead of using *ugly SWITCH statement
- * @param {*} cases
- */
-const identifyCalculation = cases => dafultFn => key =>
- key in cases ? cases[key] : dafultFn
-
- /**
-  * Gettting amount percentage
-  * @param {*} product
-  */
-const calculatePercentage = (product) =>
-  getTotalPrice(
-    parseFloat(product.get('price')),
-    parseFloat(product.getIn(['discount', 'value']))
-  )
-
-/**
-  * Gettting amount amount
-  * @param {*} product
-  */
-const calculateAmount = (product) =>
-    parseFloat(product.get('price')) - parseFloat(product.getIn(['discount', 'value']))
-
-/**
- * Main component for getting the price
- * @param {*} param0
- */
-const calculateProductPrice = (product) => {
-  if (product.get('discount').size === 0) {
-    return (parseFloat(product.get('price')).toLocaleString())
-  }
-
-  return identifyCalculation({
-    PERCENTAGE: calculatePercentage,
-    AMOUNT: calculateAmount
-  })(calculatePercentage)(product.getIn(['discount', 'discountType']))(product)
-}
+import { calculateProductPrice } from 'utils/promo'
 
 function ProductView ({
   loader,
