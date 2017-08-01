8a0df0e863a5eda1e6aa04f9c1b4b41a7d5a8e0a
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-08-01 14:06:46 +0800

    Put back the clearInterval

--- app/components/Countdown/index.js
+++ app/components/Countdown/index.js
@@ -77,7 +77,7 @@ class Countdown extends React.PureComponent { // eslint-disable-line react/prefe
     const [ hh, mm, ss ] = timer.split(':')
 
     if ((pT(hh) + pT(mm) + pT(ss)) < 0) {
-      // clearInterval(this.countdownInterval)
+      clearInterval(this.countdownInterval)
       this.setState({
         timer: '00:00:00'
       })
