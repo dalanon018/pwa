b1e0fcc15cee91b5f5224f03ff665a7e57fa5b0e
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2017-09-07 18:24:06 +0800

    static ogs

--- app/containers/ProductPage/index.js
+++ app/containers/ProductPage/index.js
@@ -204,7 +204,7 @@ export class ProductPage extends React.PureComponent { // eslint-disable-line re
             { property: 'og:url', content: `${window.location.href}` },
             { property: 'og:title', content: `${product.get('title')}` },
             { property: 'og:type', content: 'product' },
-            { property: 'og:description', content: `${product.get('details')}` },
+            { property: 'og:description', content: 'kdsfjdslkfjdslkjdslkfjdsldsj' },
             { property: 'og:image', content: `${product.get('image')}` }
           ]}
         />
