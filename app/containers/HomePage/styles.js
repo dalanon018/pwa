afc75724f75d4b3690d278dc6f09901dfb0cb735
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-10-05 18:42:13 +0800

    Fix lint

--- app/containers/HomePage/styles.js
+++ app/containers/HomePage/styles.js
@@ -5,29 +5,7 @@ const BannerWrapper = styled.div`
     padding: 0 14px;
   }
 `
-const ContentWrapper = styled.div`
-  @media(min-width: 768px) {
-    .header-label {
-      padding: 35px 15px 14px;
-    }
-
-    .header-label span {
-      font-size: 20px;
-    }
-  }
-`
-
-const SearchWrapper = styled.div`
-  background-color: #F0F0F0;
-  padding: 6px 14px;
-
-  .input {
-    width: 100%;
-  }
-`
 
 export {
-  BannerWrapper,
-  ContentWrapper,
-  SearchWrapper
+  BannerWrapper
 }
