bc64acb4da33465e4f41c7597e7ec53af636de97
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2017-10-09 14:16:39 +0800

    fix accordion, modified changeroute

--- app/components/ChildAccordion/index.js
+++ app/components/ChildAccordion/index.js
@@ -13,21 +13,49 @@ import {
   ListCollapseWrapper
 } from './styles'
 
-const ChildAccordion = ({ title, children }) => (
-  <ListCollapseWrapper>
-    <Accordion>
-      <Accordion.Title className='child-accordion'>
-        { title }
-        <i className='icon' />
-      </Accordion.Title>
-      <Accordion.Content>
-        <div className='collapse-content'>
-          { children }
-        </div>
-      </Accordion.Content>
-    </Accordion>
-  </ListCollapseWrapper>
-)
+class ChildAccordion extends React.PureComponent {
+  constructor () {
+    super()
+    this.state = {
+      activeIndex: null
+    }
+  }
+
+  _handleClick = (e, data) => {
+    const { index } = data
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    const element = e.target.closest('.ui.accordion')
+    const block = element.getElementsByClassName('content')
+
+    this.setState({
+      activeIndex: newIndex,
+      height: block[0].firstChild.offsetHeight
+    })
+  }
+
+  render () {
+    const { title, children } = this.props
+    const { activeIndex } = this.state
+
+    return (
+      <ListCollapseWrapper>
+        <Accordion>
+          <Accordion.Title className='child-accordion' onClick={this._handleClick} active={activeIndex === 0} index={0}>
+            { title }
+            <i className='icon' />
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            <div className='collapse-content'>
+              { children }
+            </div>
+          </Accordion.Content>
+        </Accordion>
+      </ListCollapseWrapper>
+    )
+  }
+}
 
 ChildAccordion.propTypes = {
   title: PropTypes.oneOfType([
