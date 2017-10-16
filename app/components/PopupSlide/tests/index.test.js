7a42fd57ac9818bf24d71bb404e61fee43053022
Benison Bolanos <ben@offshorly.com>
2017-10-16 18:58:58 +0800

    created tests

--- app/components/PopupSlide/tests/index.test.js
+++ app/components/PopupSlide/tests/index.test.js
@@ -3,6 +3,24 @@ import { shallow } from 'enzyme'
 
 import { PopupSlide } from '../index'
 
+import CloseButton from 'components/CloseButton'
+import Input from 'components/InputField'
+import Checkbox from 'components/CheckboxField'
+import Modal from 'components/PromptModal'
+import A from 'components/A'
+import H1 from 'components/H1'
+import { LoadingStateInfo } from 'components/LoadingBlock'
+
+import {
+  PopupWrapper,
+  PopupContainer,
+  InputWrapper,
+  BannerHeader,
+  TextWrapper,
+  PopupContent,
+  TermsConditionsWrapper,
+  ButtonWrapper } from '../styles'
+
 const wrapper = (props = {}, enzyme = shallow) => shallow(
   <PopupSlide {...props} />
 )
@@ -23,5 +41,80 @@ describe('<PopupSlide />', () => {
     expect(
       renderComponent.length
     ).toEqual(1)
+  })
+
+  it('renders one <PopupWrapper/> styled component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(PopupWrapper)).toHaveLength(1)
+  })
+
+  it('renders one <BannerHeader/> styled component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(BannerHeader)).toHaveLength(1)
+  })
+
+  it('renders one <PopupContainer/> styled component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(PopupContainer)).toHaveLength(1)
+  })
+
+  it('renders one <PopupContent/> styled component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(PopupContent)).toHaveLength(1)
+  })
+
+  it('renders one <TextWrapper/> styled component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(TextWrapper)).toHaveLength(1)
+  })
+
+  it('renders one <InputWrapper/> styled component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(InputWrapper)).toHaveLength(1)
+  })
+
+  it('renders one <TermsConditionsWrapper/> styled component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(TermsConditionsWrapper)).toHaveLength(1)
+  })
+
+  it('renders one <ButtonWrapper/> styled component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(ButtonWrapper)).toHaveLength(1)
+  })
+
+  it('renders one <CloseButton/> component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(CloseButton)).toHaveLength(1)
+  })
+
+  it('renders one <Input/> component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(Input)).toHaveLength(1)
+  })
+
+  it('renders one <Checkbox/> component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(Checkbox)).toHaveLength(1)
+  })
+
+  it('renders one <Modal/> component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(Modal)).toHaveLength(1)
+  })
+
+  it('renders zero <A/> component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(A)).toHaveLength(0)
+  })
+
+  it('renders one <H1/> component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(H1)).toHaveLength(1)
+  })
+
+  it('renders one <LoadingStateInfo/> component', () => {
+    const renderComponent = wrapper(minProps)
+    expect(renderComponent.find(LoadingStateInfo)).toHaveLength(1)
   })
 })
--- app/components/Product/tests/index.test.js
+++ app/components/Product/tests/index.test.js
@@ -8,6 +8,22 @@ import { fromJS } from 'immutable'
 
 import Product from '../index'
 
+import ProductSlider from 'components/BannerSlider'
+import {LoadingStateInfo} from 'components/LoadingBlock'
+
+import {
+  ButtonContainer,
+  DetailsWrapper,
+  ProductDetails,
+  ProductMainContent,
+  ProductPriceWrapper,
+  ProductWrapper,
+  SocialContainer,
+  ShareWrapper,
+  ProductImageSlider,
+  CollapseContent
+} from '../styled'
+
 const children = 'Test'
 const wrapper = (props = {}, enzyme = shallow) => enzyme(
   <Product {...props}>
@@ -38,5 +54,65 @@ describe('<Product />', () => {
     expect(
       renderComponent.length
     ).toEqual(1)
+  })
+
+  it('renders one <ProductImageSlider/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(ProductImageSlider)).toHaveLength(1)
+  })
+
+  it('renders one <ProductSlider/> component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(ProductSlider)).toHaveLength(1)
+  })
+
+  it('renders one <ProductMainContent/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(ProductMainContent)).toHaveLength(1)
+  })
+
+  it('renders two <LoadingStateInfo/> components', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(LoadingStateInfo)).toHaveLength(2)
+  })
+
+  it('renders one <ProductPriceWrapper/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(ProductPriceWrapper)).toHaveLength(1)
+  })
+
+  it('renders one <SocialContainer/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(SocialContainer)).toHaveLength(1)
+  })
+
+  it('renders one <ShareWrapper/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(ShareWrapper)).toHaveLength(1)
+  })
+
+  it('renders one <DetailsWrapper/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(DetailsWrapper)).toHaveLength(1)
+  })
+
+  it('renders one <ProductDetails/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(ProductDetails)).toHaveLength(1)
+  })
+
+  it('renders one <ButtonContainer/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(ButtonContainer)).toHaveLength(1)
+  })
+
+  it('renders one <ProductWrapper/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(ProductWrapper)).toHaveLength(1)
+  })
+
+  it('renders three <CollapseContent/> styled component', () => {
+    const ShallowedWrapper = shallow(<Product {...minProps} />)
+    expect(ShallowedWrapper.find(CollapseContent)).toHaveLength(1)
   })
 })
