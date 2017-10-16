e9e1bf8edd98bbfd9d8c06c158d83b148b579cfb
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-10-16 12:07:12 +0800

    Added test

--- app/components/LazyLoading/tests/index.test.js
+++ app/components/LazyLoading/tests/index.test.js
@@ -1,10 +1,35 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react'
+import { fromJS } from 'immutable'
+import { shallow } from 'enzyme'
 
-// import LazyLoading from '../index';
+import LazyLoading from '../index'
 
-describe('<LazyLoading />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false)
+const Children = () => (<h1> Lazyloading </h1>)
+const wrapper = (props = {}, enzyme = shallow) => shallow(
+  <LazyLoading {...props}>
+    <Children />
+  </LazyLoading>
+)
+
+describe('<Brand />', () => {
+  const minProps = {
+    lazyload: false,
+    results: fromJS([]),
+    onScroll: () => {},
+    limit: 0
+  }
+
+  it('render without exploding', () => {
+    const renderComponent = wrapper(minProps)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
+  })
+
+  it('should render children', () => {
+    const renderComponent = wrapper(minProps)
+    expect(
+      renderComponent.find(Children).length
+    ).toEqual(1)
   })
 })
