763127a34a670e3d31226d7a89d4579a289ac5a6
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2017-10-27 16:39:05 +0800

    remove trailing spaces

--- app/components/Receipt/index.js
+++ app/components/Receipt/index.js
@@ -339,7 +339,7 @@ class Receipt extends React.PureComponent {
   render () {
     const { show } = this.state
     const { timer, receipt, statuses } = this.props
-    
+
     return (
       <div>
         <ReceiptWrapper>
