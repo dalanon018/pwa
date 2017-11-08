16409e4f7f74049ff965bc40cef257decc0d719c
Benison Bolanos <ben@offshorly.com>
2017-11-08 15:24:09 +0800

    fixed standards

---
+++ app/components/BannerSlider/SlideShow.js
@@ -0,0 +1,69 @@
+/**
+*
+* BannerSlider
+*
+*/
+
+import React, { PropTypes } from 'react'
+import Slider from 'react-slick'
+import { Image } from 'semantic-ui-react'
+
+import { ifElse, gt } from 'ramda'
+import { noop } from 'lodash'
+
+class SlideShow extends React.PureComponent {
+  static propTypes = {
+    isLowerdots: PropTypes.bool,
+    isInfinite: PropTypes.bool,
+    images: PropTypes.array.isRequired,
+    slidesToShow: PropTypes.number,
+    settings: PropTypes.object.isRequired
+  }
+
+  assignTimeOut
+
+  _initNextSlide = () => {
+    const { innerSlider } = this.slider
+
+    this.assignTimeOut = setTimeout(() => {
+      innerSlider.__isMounted && innerSlider.slickNext()
+    }, 3500)
+  }
+
+  _clearTimeOut = () => {
+    clearTimeout(this.assignTimeOut)
+  }
+
+  componentDidMount () {
+    const { props } = this.slider
+    const shouldSlideNext = ifElse(gt(props.children.length), this._initNextSlide, noop)
+
+    shouldSlideNext(1)
+  }
+
+  render () {
+    const { settings, images } = this.props
+
+    return (
+      <Slider
+        ref={c => { this.slider = c }}
+        {...settings}
+        afterChange={this._clearTimeOut}>
+        {
+          images &&
+          images.map((item, index) => {
+            return (
+              <div key={index}>
+                {
+                  (typeof item === 'string' ? <Image alt='Cliqq' src={item} /> : '')
+                }
+              </div>
+            )
+          })
+        }
+      </Slider>
+    )
+  }
+}
+
+export default SlideShow
