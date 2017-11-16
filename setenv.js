2f90aa57179c0693a7ba5efb7150aefe3ee6e700
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-11-16 17:02:47 +0800

    Fix set-env added env-cmd

--- setenv.js
+++ setenv.js
@@ -4,6 +4,7 @@ if (env === 'production') {
   console.log(`TOKEN_URL=${process.env.TOKEN_URL}`)
   console.log(`API_BASE_URL=${process.env.API_BASE_URL}`)
   console.log(`APP_BASE_URL=${process.env.APP_BASE_URL}`)
+  console.log(`MOBILE_REGISTRATION_URL=${process.env.MOBILE_REGISTRATION_URL}`)
   console.log(`STORE_LOCATOR_URL=${process.env.STORE_LOCATOR_URL}`)
 
   console.log(`FIREBASE_API_KEY=${process.env.FIREBASE_API_KEY}`)
@@ -28,6 +29,7 @@ if (env === 'production') {
   console.log(`TOKEN_URL=https://apidemo.cliqq.net:8443/accounts/oauth2/token`)
   console.log(`API_BASE_URL=https://apidemo.cliqq.net:8443/ecms/api/v1`)
   console.log(`APP_BASE_URL=http://localhost:3000`)
+  console.log(`MOBILE_REGISTRATION_URL=https://apidemo.cliqq.net:8443`)
   console.log(`STORE_LOCATOR_URL=https://mapservice.cliqq.net/ajax/`)
 
   console.log(`FIREBASE_API_KEY=AIzaSyBaKhRjF4feFUsPyezk8Hmu0u_PCbNb2oI`)
@@ -46,5 +48,5 @@ if (env === 'production') {
   console.log(`OATH_RESPONSE_TYPE=token`)
   console.log(`OATH_GRANT_TYPE=client_credentials`)
 
-  console.log(`RECAPTCHA_SITE_KEY=Le2aDEUAAAAAC_B5o0Xy4_DAAb7Cmvsdrd4ury0`)
+  console.log(`RECAPTCHA_SITE_KEY=6Le2aDEUAAAAAC_B5o0Xy4_DAAb7Cmvsdrd4ury0`)
 }
