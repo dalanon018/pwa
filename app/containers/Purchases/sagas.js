c06d60f4e1ee812c311216ab2b98964c88549507
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-11-24 13:46:04 +0800

    make sure that name is correct

--- app/containers/Purchases/sagas.js
+++ app/containers/Purchases/sagas.js
@@ -83,7 +83,7 @@ function * findAndUpdateReceiptDetails (apiOrders) {
     const trackingNumber = prop('trackingNumber', oldReceipt)
     const findReceipt = find(propEq('trackingNumber', trackingNumber))
     const updateOldReceipt = (data) => compose(
-      assoc('product', propOr({}, 'product', data)),
+      assoc('products', propOr({}, 'products', data)),
       assoc('claimDate', propOr('', 'claimDate', data)),
       assoc('lastUpdated', prop('lastUpdated', data)),
       assoc('status', prop('status', data)),
