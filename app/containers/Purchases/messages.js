1ba3b91664ddd8be35761c3af8684475b2075f02
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-11-24 12:15:29 +0800

    Fix purchases messages id

--- app/containers/Purchases/messages.js
+++ app/containers/Purchases/messages.js
@@ -11,15 +11,15 @@ export default defineMessages({
     defaultMessage: 'RECEIPTS'
   },
   emptyActivePurchases: {
-    id: 'app.containers.Purchases.emptyPurchases',
+    id: 'app.containers.Purchases.emptyActivePurchases',
     defaultMessage: 'Woops! It seems like you haven\'t bought anything yet. Let\'s fix that!'
   },
   emptyCompletedPurchases: {
-    id: 'app.containers.Purchases.emptyPurchases',
+    id: 'app.containers.Purchases.emptyCompletedPurchases',
     defaultMessage: 'Woops! It seems like you haven\'t bought anything yet. Let\'s fix that!'
   },
   emptyExpiredPurchases: {
-    id: 'app.containers.Purchases.emptyPurchases',
+    id: 'app.containers.Purchases.emptyExpiredPurchases',
     defaultMessage: 'There are no expired receipts.'
   },
   // Here we will define the status titles
