10597e005d3deb6f1b1febbbc6462802558b5f1d
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-12-14 15:30:24 +0800

    fix test

--- app/components/PageLoading/tests/index.test.js
+++ app/components/PageLoading/tests/index.test.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { render } from 'enzyme'
+import { shallow } from 'enzyme'
 
 import PageLoading from '../index'
 
+const wrapper = (props = {}, enzyme = shallow) => shallow(
+  <PageLoading {...props} />
+)
+
 describe('<PageLoading />', () => {
-  it('should render without exploding', () => {
-    const renderedComponent = render(
-      <PageLoading />
-    )
-    expect(renderedComponent.length).toEqual(1)
+  const minProps = {
+    intl: {
+      formatMessage: () => {}
+    }
+  }
+  it('render without exploding', () => {
+    const renderComponent = wrapper(minProps)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
   })
 })
