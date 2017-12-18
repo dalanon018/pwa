72968ad52fbb3889aaccf98243ac84840baac7c4
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-12-18 13:27:43 +0800

    Fix issue with uri malform on redux-auth-wrapper location helper, redirect user to homepage if uri malform detected, fix-7-ELEVEN-FRON-END-2Q

--- app/containers/App/auth.js
+++ app/containers/App/auth.js
@@ -1,11 +1,19 @@
 import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect'
-import locationHelperBuilder from 'redux-auth-wrapper/history4/locationHelper'
+import { compose, propOr } from 'ramda'
 
-export const locationHelper = locationHelperBuilder({})
+import { fnQueryObject } from 'utils/http'
 
 export const defaultProps = {
   // This sends the user either to the query param route if we have one, or to the landing page if none is specified and the user is already logged in
-  redirectPath: (state, ownProps) => locationHelper.getRedirectQueryParam(ownProps) || '/'
+  redirectPath: (state, ownProps) => {
+    const { location: { search } } = ownProps
+    const queryString = compose(
+      propOr('/', 'redirect'),
+      fnQueryObject
+    )
+
+    return queryString(search) || '/'
+  }
 }
 
 export const userIsAuthenticated = connectedRouterRedirect({
