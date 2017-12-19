253d7fb9b8f2b7082aec88c7bc0e763a3b84bce1
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-12-19 16:30:56 +0800

    Fix issue on product review can be submitted even orderedProduct is not loaded yet

--- app/containers/ProductReview/index.js
+++ app/containers/ProductReview/index.js
@@ -218,7 +218,7 @@ export class ProductReview extends React.PureComponent { // eslint-disable-line 
     )
 
     this.submitting = true
-    return proceedOrder(modePayment)
+    return (orderedProduct.size > 0) ? proceedOrder(modePayment) : null
   }
 
   _handleStoreLocator () {
