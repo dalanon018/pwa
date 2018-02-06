25eaa42cdda78c778cfd1f4bf2fa63cbcaa0aaa1
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-02-06 17:14:12 +0800

    Commented out hoverflow hidden since it will cut the page if height is 100%

--- app/containers/Buckets/index.js
+++ app/containers/Buckets/index.js
@@ -105,8 +105,10 @@ const Wrapper = styled.div`
 
 const MainContent = styled.div`
   margin-top: 50px;
-  overflow: hidden;
+  // overflow: hidden;
   width: 100%;
+  height: 100%;
+  min-height: 100%;
 `
 
 export class Buckets extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
