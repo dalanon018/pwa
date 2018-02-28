55ba29b4f6ed2990b45566f38f21c3e9bb210d45
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-02-28 16:40:10 +0800

    Fix tests

--- app/components/InfiniteLoading/tests/index.test.js
+++ app/components/InfiniteLoading/tests/index.test.js
@@ -8,17 +8,26 @@ import { InfiniteLoading } from '../index'
 const Children = () => (<h1> InfiniteLoading </h1>)
 const wrapper = (props = {}, enzyme = shallow) => shallow(
   <InfiniteLoading {...props}>
-    <Children />
+    {() => (
+      <Children />
+    )}
   </InfiniteLoading>
 )
 
 describe('<InfiniteLoading />', () => {
   const minProps = {
+    // to check if we are currently loading
     isLoading: false,
-    lazyload: false,
+    // to check if we still need to scroll
+    // we need there are still data to be fetched
+    hasMoreData: false,
+    // callback on scroll
+    loadMoreData: () => {},
+    // children to load
+    children: () => {},
     results: fromJS([]),
-    onScroll: () => {},
-    limit: 0
+    // total row count
+    rowCount: 0
   }
 
   it('render without exploding', () => {
@@ -28,10 +37,11 @@ describe('<InfiniteLoading />', () => {
     ).toEqual(1)
   })
 
-  it('should render children', () => {
+  it('should render InfiniteLoader', () => {
     const renderComponent = wrapper(minProps)
+
     expect(
-      renderComponent.find(Children).length
+      renderComponent.find('InfiniteLoader').length
     ).toEqual(1)
   })
 })
