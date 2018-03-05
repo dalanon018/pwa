9898121426b6d3d25ad6ee7752f7a63ab2a82b52
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-05 14:55:21 +0800

    Added test for timer component

--- app/components/Shared/CountDownTimer/tests/index.test.js
+++ app/components/Shared/CountDownTimer/tests/index.test.js
@@ -1,32 +1,20 @@
 import React from 'react'
-import { fromJS } from 'immutable'
+import moment from 'moment'
 import { shallow } from 'enzyme'
 
 import Countdown from '../index'
 
-const TestInnerComponent = (props) => <div {...props} />
-
-const wrapper = (Component, props = {}, enzyme = shallow) => enzyme(
-  <Component {...props} />
+const wrapper = (props = {}, enzyme = shallow) => enzyme(
+  <Countdown {...props} />
 )
 
 describe('<Countdown />', () => {
   const minProps = {
-    statuses: {},
-    receipt: fromJS({})
+    endDate: moment().add(1, 'hours').format('YYY-MM-DD HH:mm:ss')
   }
 
-  it('render inner component without exploding', () => {
-    const renderComponent = wrapper(TestInnerComponent)
-    expect(
-      renderComponent.length
-    ).toEqual(1)
-  })
-
-  it('should render the HOC without exploding', () => {
-    const HOC = Countdown(TestInnerComponent)
-    const renderComponent = wrapper(HOC, minProps)
-
+  it('render component without exploding', () => {
+    const renderComponent = wrapper(minProps)
     expect(
       renderComponent.length
     ).toEqual(1)
