5dbca7d300cc7c136058835948c2606d0b06b894
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2018-03-07 15:01:23 +0800

    modified brand slider and default icon nav cat

--- app/components/Mobile/BrandSlider/index.js
+++ app/components/Mobile/BrandSlider/index.js
@@ -17,25 +17,55 @@ import PlainCard from 'components/Mobile/PlainCard'
 
 import TestBrand from 'images/test-images/best-foods.png'
 
-import Carousel from 'react-slick'
-
 import { imageStock } from 'utils/image-stock'
 
 // import { FormattedMessage } from 'react-intl'
 // import messages from './messages'
 
+export const Wrapper = styled.div`
+  display: block;
+  margin-bottom: -15px;
+  position: relative;
+  width: 100%;
+`
+
 export const BrandsWrapper = styled.div`
+  overflow-y: hidden;
+  width: ${props => (94 * props.size) + (10 * props.size) - 10}px;
+
   .slick-list {
     padding: 2px;
   }
 `
 
-const SliderItem = styled.div`
+export const BrandsContainer = styled.div`
+  height: 100%;
+  overflow-x: auto;
+  overflow-y: hidden;
+  padding-bottom: 15px;
+`
+
+export const HideScroll = styled.div`
+  background-color: #f4f4f4;
+  bottom: 0;
+  height: 15px;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  z-index: 1;
+`
+
+export const SliderItem = styled.div`
   margin-right: 10px;
+  display: inline-block;
 
   // temporary
   img {
     width: 90px;
+  }
+
+  &:last-child {
+    margin-right: 0;
   }
 `
 
@@ -50,33 +80,29 @@ const imgixOptions = {
 function BrandSlider ({ brands, loader, changeRoute }) {
   const goToBrand = (id) => () => changeRoute(`/brands/${id}`)
 
-  var settings = {
-    arrows: false,
-    infinite: false,
-    slidesToShow: 1,
-    speed: 800,
-    swipeToSlide: true,
-    variableWidth: true
-  }
-
   return (
     <Container>
-      <BrandsWrapper>
-        <Carousel {...settings}>
-          {
-            loader ? range(4).map((_, index) => <SliderItem key={index}><DefaultState /></SliderItem>)
-            : brands.map((brand, index) => {
-              return (
-                <SliderItem key={index} onClick={goToBrand(brand.get('id'))}>
-                  <PlainCard size={94}>
-                    <Image src={TestBrand} alt='CLiQQ' />
-                  </PlainCard>
-                </SliderItem>
-              )
-            })
-          }
-        </Carousel>
-      </BrandsWrapper>
+      <Wrapper>
+        <BrandsContainer>
+          <div>
+            <HideScroll />
+            <BrandsWrapper size={brands.size ? brands.size : 4}>
+              {
+                loader ? range(4).map((_, index) => <SliderItem key={index}><DefaultState /></SliderItem>)
+                : brands.map((brand, index) => {
+                  return (
+                    <SliderItem key={index} onClick={goToBrand(brand.get('id'))}>
+                      <PlainCard size={94}>
+                        <Image src={TestBrand} alt='CLiQQ' />
+                      </PlainCard>
+                    </SliderItem>
+                  )
+                })
+              }
+            </BrandsWrapper>
+          </div>
+        </BrandsContainer>
+      </Wrapper>
     </Container>
   )
 }
