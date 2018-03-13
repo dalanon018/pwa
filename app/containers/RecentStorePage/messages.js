7738412a4b93f20206074790f92de4443f6c54b8
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-13 16:47:25 +0800

    Updated header name

--- app/containers/RecentStorePage/messages.js
+++ app/containers/RecentStorePage/messages.js
@@ -8,6 +8,6 @@ import { defineMessages } from 'react-intl'
 export default defineMessages({
   header: {
     id: 'app.containers.RecentStorePage.header',
-    defaultMessage: 'Categories'
+    defaultMessage: 'Select a Store'
   }
 })
