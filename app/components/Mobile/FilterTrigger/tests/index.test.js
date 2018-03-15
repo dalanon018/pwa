6ccf169159a4e2c692963fda88e7378b0569bf4c
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-15 23:18:34 +0800

    Fix tests

--- app/components/Mobile/FilterTrigger/tests/index.test.js
+++ app/components/Mobile/FilterTrigger/tests/index.test.js
@@ -1,10 +1,30 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react'
 
-// import FilterTrigger from '../index';
+import { fromJS } from 'immutable'
+import { shallow } from 'enzyme'
+
+import FilterTrigger from '../index'
+
+const wrapper = (props = {}, enzyme = shallow) => shallow(
+  <FilterTrigger {...props} />
+)
 
 describe('<FilterTrigger />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true)
+  const minProps = {
+    parentId: '1',
+    getFilterCategories: () => {},
+    requestFromFilter: () => {},
+    getFilterBrands: () => {},
+    filterCategories: fromJS({}),
+    filterBrands: fromJS({}),
+    filterCategoriesLoading: false,
+    filterBrandsLoading: false
+  }
+
+  it('render without exploding', () => {
+    const renderComponent = wrapper(minProps)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
   })
 })
