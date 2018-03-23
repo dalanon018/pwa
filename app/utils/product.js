3ecc47cf61679dcba6719c1eac6f44077f5f07d3
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-23 14:37:33 +0800

    Created helper for centrailize usage for calculation and product pricing

---
+++ app/utils/product.js
@@ -0,0 +1,37 @@
+
+import {
+  multiply,
+  divide,
+  subtract
+} from 'ramda'
+
+/**
+ * helper on which of the property use for the price
+ * // so we can expand this.
+ * @param {*} product
+ */
+export const toggleOrigDiscountPrice = (product) => {
+  const showPrice = product.get('discountPrice') || product.get('price')
+
+  return showPrice ? showPrice.toLocaleString() : 0
+}
+
+export const computeTotalPointsPrice = (product) => {
+  const amount = toggleOrigDiscountPrice(product)
+  const multiplier = product.getIn(['points', 'multiplier'])
+  return Math.ceil(multiply(amount, multiplier))
+}
+
+export const calculatePricePoints = ({
+  product,
+  usePoints,
+}) => {
+  const pointsAmount = computeTotalPointsPrice(product)
+  const pointsMultiplier = product.getIn(['points', 'multiplier'])
+  const calculate = Math.floor(divide(
+    subtract(pointsAmount, usePoints),
+    pointsMultiplier
+  ))
+  // make sure not NAN
+  return calculate || 0
+}
