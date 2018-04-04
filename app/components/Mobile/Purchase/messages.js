dc5b6702ea84643860bbd8a59fa5c351ef231ff0
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-04-04 10:41:58 +0800

    Updated locale for points and cash payment

--- app/components/Mobile/Purchase/messages.js
+++ app/components/Mobile/Purchase/messages.js
@@ -14,6 +14,7 @@ export default defineMessages({
     id: `app.components.Purchase.peso`,
     defaultMessage: '₱'
   },
+  // CASH
   dateCASHRESERVED: {
     id: 'app.components.Purchase.dateCASHRESERVED',
     defaultMessage: 'Time Remaining'
@@ -50,6 +51,7 @@ export default defineMessages({
     id: 'app.components.Purchase.dateCASHUNCLAIMED',
     defaultMessage: 'Date Expired'
   },
+  // COD
   dateCODRESERVED: {
     id: 'app.components.Purchase.dateCODRESERVED',
     defaultMessage: 'Date Ordered'
@@ -90,12 +92,57 @@ export default defineMessages({
     id: 'app.components.Purchase.dateCODUNCLAIMED',
     defaultMessage: 'Date Expired'
   },
+  // POINTS_CASH
+  datePOINTS_CASHRESERVED: {
+    id: 'app.components.Purchase.datePOINTS_CASHRESERVED',
+    defaultMessage: 'Date Ordered'
+  },
+  datePOINTS_CASHRESERVEDEXPIRED: {
+    id: 'app.components.Purchase.datePOINTS_CASHRESERVEDEXPIRED',
+    defaultMessage: 'Time Remaining'
+  },
+  datePOINTS_CASHUNPAID: {
+    id: 'app.components.Purchase.datePOINTS_CASHUNPAID',
+    defaultMessage: 'Date Expired'
+  },
+  datePOINTS_CASHPROCESSING: {
+    id: 'app.components.Purchase.datePOINTS_CASHPROCESSING',
+    defaultMessage: 'Date Ordered'
+  },
+  datePOINTS_CASHCONFIRMED: {
+    id: 'app.components.Purchase.datePOINTS_CASHCONFIRMED',
+    defaultMessage: 'Date Ordered'
+  },
+  datePOINTS_CASHINTRANSIT: {
+    id: 'app.components.Purchase.datePOINTS_CASHINTRANSIT',
+    defaultMessage: 'Date In-transit'
+  },
+  datePOINTS_CASHLOSTINTRANSIT: {
+    id: 'app.components.Purchase.datePOINTS_CASHLOSTINTRANSIT',
+    defaultMessage: 'Date Unserved'
+  },
+  datePOINTS_CASHDELIVERED: {
+    id: 'app.components.Purchase.dateDELIVERED',
+    defaultMessage: 'Date Delivered'
+  },
+  datePOINTS_CASHCLAIMED: {
+    id: 'app.components.Purchase.datePOINTS_CASHCLAIMED',
+    defaultMessage: 'Date Claimed'
+  },
+  datePOINTS_CASHUNCLAIMED: {
+    id: 'app.components.Purchase.datePOINTS_CASHUNCLAIMED',
+    defaultMessage: 'Date Expired'
+  },
   dateCASHFieldDefault: {
     id: 'app.components.Purchase.dateCASHFieldDefault',
     defaultMessage: 'Date'
   },
   dateCODFieldDefault: {
     id: 'app.components.Purchase.dateCODFieldDefault',
+    defaultMessage: 'Date'
+  },
+  datePOINTS_CASHFieldDefault: {
+    id: 'app.components.Purchase.datePOINTS_CASHFieldDefault',
     defaultMessage: 'Date'
   }
 })
