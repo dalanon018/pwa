cca841039ea3647df5abddb4c2fc21abf0807e1a
Noel Alfonso Miranda <noelalfonsomiranda@gmail.com>
2018-05-02 18:45:03 +0800

    added ordertipmodal component

---
+++ app/components/Mobile/OrderTipModal/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import OrderTipModal from '../index';
+
+describe('<OrderTipModal />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(true)
+  })
+})
