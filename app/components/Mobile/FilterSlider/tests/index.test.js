8a28d7c081a4c7c275e40ed5d9e0d5363c362e63
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2018-05-08 15:15:54 +0800

    enhance mobile filterslider unit test

--- app/components/Mobile/FilterSlider/tests/index.test.js
+++ app/components/Mobile/FilterSlider/tests/index.test.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 import { shallow } from 'enzyme'
 
-import FilterSlider from '../index'
+import FilterSlider, {
+  Wrapper,
+  OptionWrapper,
+  ButtonWrapper,
+  FormWrapper,
+  BlockWrapper } from '../index'
 
 const wrapper = (props = {}, context = {}, enzyme = shallow) => shallow(
   <FilterSlider {...props} />,
@@ -29,6 +34,41 @@ describe('<FilterSlider />', () => {
 
   it('render without exploding', () => {
     const renderComponent = wrapper(minProps, context)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
+  })
+
+  it('Wrapper should render', () => {
+    const renderComponent = shallow(<Wrapper />)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
+  })
+
+  it('OptionWrapper should render', () => {
+    const renderComponent = shallow(<OptionWrapper />)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
+  })
+
+  it('ButtonWrapper should render', () => {
+    const renderComponent = shallow(<ButtonWrapper />)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
+  })
+
+  it('FormWrapper should render', () => {
+    const renderComponent = shallow(<FormWrapper />)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
+  })
+
+  it('BlockWrapper should render', () => {
+    const renderComponent = shallow(<BlockWrapper />)
     expect(
       renderComponent.length
     ).toEqual(1)
