4efded17337ef24ef85a511cf4ddbe9fe02c5584
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-05-15 15:40:58 +0800

    Update test make sure to toggle component correctly

--- app/components/Mobile/Product/tests/index.test.js
+++ app/components/Mobile/Product/tests/index.test.js
@@ -3,14 +3,17 @@
  */
 
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import { fromJS } from 'immutable'
 
-import Product from '../index'
-import SizeSelector from '../SizeSelector'
+import { intl } from 'utils/test-helper'
 
 import ProductSlider from 'components/Shared/BannerSlider'
-import {LoadingStateInfo} from 'components/Shared/LoadingBlock'
+import { LoadingStateInfo } from 'components/Shared/LoadingBlock'
+
+import Product, { ToggleFullPoints, ToggleEarnPoints } from '../index'
+import SizeSelector from '../SizeSelector'
+import messages from '../messages'
 
 import {
   ButtonContainer,
@@ -18,11 +21,13 @@ import {
   ProductDetails,
   ProductMainContent,
   ProductPriceWrapper,
+  FullPointsWrapper,
   ProductWrapper,
   SocialContainer,
   ShareWrapper,
   ProductImageSlider,
-  CollapseContent
+  CollapseContent,
+  PointsInfo
 } from '../styled'
 
 const children = 'Test'
@@ -31,77 +36,79 @@ const wrapper = (props = {}, enzyme = shallow) => enzyme(
     {children}
   </Product>
 )
-
-describe('<Product />', () => {
-  const minProps = {
-    product: fromJS({
+const mockData = {
+  'title': 'Penshoppe All Day Sling Bag (Blue)',
+  'details': '<p>Men&rsquo;s all day sling bag in 100% polyester coated polycanvas with color blocking pattern, and 2 compartments</p>',
+  'barcode': '31005231',
+  'additionalDetails': 'This is a sample text for the additional details field for this product. Thank you',
+  'isFeatured': true,
+  'returnable': true,
+  'returnPolicy': 'Not satisfied with your purchase? Depending on our CLiQQ Care Policy, you can return your item anytime of the day within 7 days from claim date. Just go to the 7-Eleven store where the item was purchased and get your Return Slip from the CLiQQ Kiosk and bring it to the cashier.',
+  'brand': {
+    'code': '01',
+    'name': 'Penshoppe',
+    'isFeatured': true,
+    'images': [{
+      'imageUrl': 'https://cliqq.imgix.net/test/category/product-assets/brand-logo.png', 'imageType': 'BRAND_LOGO'},
+      {'imageUrl': 'http://i.dailymail.co.uk/i/pix/2015/05/07/16/286A073200000578-0-image-a-2_1431013568311.jpg', 'imageType': 'BACKGROUND'},
+      {'imageUrl': 'http://ph-live-01.slatic.net/cms/category_banner/02152016/kendall.jpg', 'imageType': 'SLIDER'},
+      {'imageUrl': 'http://ph-live-02.slatic.net/cms/category_banner/02152016/lucky.jpg', 'imageType': 'SLIDER'}
+    ]
+  },
+  'uom': {
+    'name': 'M',
+    'type': 'Size'
+  },
+  'association': [
+    {
       'title': 'Penshoppe All Day Sling Bag (Blue)',
       'details': '<p>Men&rsquo;s all day sling bag in 100% polyester coated polycanvas with color blocking pattern, and 2 compartments</p>',
-      'barcode': '31005231',
-      'additionalDetails': 'This is a sample text for the additional details field for this product. Thank you',
-      'isFeatured': true,
-      'returnable': true,
-      'returnPolicy': 'Not satisfied with your purchase? Depending on our CLiQQ Care Policy, you can return your item anytime of the day within 7 days from claim date. Just go to the 7-Eleven store where the item was purchased and get your Return Slip from the CLiQQ Kiosk and bring it to the cashier.',
-      'brand': {
-        'code': '01',
-        'name': 'Penshoppe',
-        'isFeatured': true,
-        'images': [{
-          'imageUrl': 'https://cliqq.imgix.net/test/category/product-assets/brand-logo.png', 'imageType': 'BRAND_LOGO'},
-          {'imageUrl': 'http://i.dailymail.co.uk/i/pix/2015/05/07/16/286A073200000578-0-image-a-2_1431013568311.jpg', 'imageType': 'BACKGROUND'},
-          {'imageUrl': 'http://ph-live-01.slatic.net/cms/category_banner/02152016/kendall.jpg', 'imageType': 'SLIDER'},
-          {'imageUrl': 'http://ph-live-02.slatic.net/cms/category_banner/02152016/lucky.jpg', 'imageType': 'SLIDER'}
-        ]
-      },
+      'quantity': '1',
+      'cliqqCode': ['000A4'],
+      'size': 'M',
       'uom': {
         'name': 'M',
         'type': 'Size'
-      },
-      'association': [
-        {
-          'title': 'Penshoppe All Day Sling Bag (Blue)',
-          'details': '<p>Men&rsquo;s all day sling bag in 100% polyester coated polycanvas with color blocking pattern, and 2 compartments</p>',
-          'quantity': '1',
-          'cliqqCode': ['000A4'],
-          'size': 'M',
-          'uom': {
-            'name': 'M',
-            'type': 'Size'
-          }
-        },
-        {
-          'title': 'Penshoppe All Day Sling Bag (Green)',
-          'details': '<p>Men&rsquo;s green all day sling bag in 100% polyester pu coated polycanvas with color blocking pattern, penshoppe logo, and 2 compartments</p>',
-          'quantity': '1',
-          'cliqqCode': ['000CB'],
-          'size': 'S',
-          'uom': {
-            'name': 'S',
-            'type': 'Size'
-          }
-        }
-      ],
-      'priceList': [
-        {
-          'amount': 139,
-          'currency': 'PHP'
-        },
-        {
-          'amount': 99,
-          'currency': 'DPHP'
-        }
-      ],
-      'cliqqCode': ['000A4'],
+      }
+    },
+    {
+      'title': 'Penshoppe All Day Sling Bag (Green)',
+      'details': '<p>Men&rsquo;s green all day sling bag in 100% polyester pu coated polycanvas with color blocking pattern, penshoppe logo, and 2 compartments</p>',
       'quantity': '1',
-      'deliveryPromiseMessage': '<p>Delivery for:</p>\n            <p>Mega Manila: 1-3 Days</p>\n            <p>Major Luzon Provinces: 3-5 Days</p>\n            <p>Vis-Min Provinces: 7-14 Days (Not available yet!)</p>',
-      'price': 139,
-      'discountPrice': 99,
-      'image': 'https://cliqq.imgix.net/000A4.png',
-      'brandLogo': 'https://cliqq.imgix.net/test/category/product-assets/brand-logo.png',
-      'sliders': [],
-      'size': 'M'
-    }),
+      'cliqqCode': ['000CB'],
+      'size': 'S',
+      'uom': {
+        'name': 'S',
+        'type': 'Size'
+      }
+    }
+  ],
+  'priceList': [
+    {
+      'amount': 139,
+      'currency': 'PHP'
+    },
+    {
+      'amount': 99,
+      'currency': 'DPHP'
+    }
+  ],
+  'cliqqCode': ['000A4'],
+  'quantity': '1',
+  'deliveryPromiseMessage': '<p>Delivery for:</p>\n            <p>Mega Manila: 1-3 Days</p>\n            <p>Major Luzon Provinces: 3-5 Days</p>\n            <p>Vis-Min Provinces: 7-14 Days (Not available yet!)</p>',
+  'price': 139,
+  'discountPrice': 99,
+  'image': 'https://cliqq.imgix.net/000A4.png',
+  'brandLogo': 'https://cliqq.imgix.net/test/category/product-assets/brand-logo.png',
+  'sliders': [],
+  'size': 'M'
+}
+
+describe('<Product />', () => {
+  const minProps = {
+    product: fromJS(mockData),
     loading: false,
+    _isFullPointsOnly: false,
     onSubmit: () => {},
     origPrice: () => {},
     intl: {
@@ -138,9 +145,92 @@ describe('<Product />', () => {
     expect(ShallowedWrapper.find(LoadingStateInfo)).toHaveLength(2)
   })
 
-  it('renders one <ProductPriceWrapper/> styled component', () => {
-    const ShallowedWrapper = shallow(<Product {...minProps} />)
-    expect(ShallowedWrapper.find(ProductPriceWrapper)).toHaveLength(1)
+  describe('Should toggle FullPoints component', () => {
+    it('renders one <ProductPriceWrapper/> if fullPoints is FALSE', () => {
+      const { product, _isFullPointsOnly } = minProps
+      const ShallowedWrapper = mount(
+        intl({
+          component: (
+            <ToggleFullPoints
+              entity={product}
+              isFullPointsOnly={_isFullPointsOnly}
+            />
+          ),
+          locale: 'en',
+          messages
+        })
+      )
+      expect(ShallowedWrapper.find(ProductPriceWrapper)).toHaveLength(1)
+    })
+    it('renders one <FullPointsWrapper/> if fullPoints is TRUE', () => {
+      const { product } = minProps
+      const ShallowedWrapper = mount(
+        intl({
+          component: (
+            <ToggleFullPoints
+              entity={product}
+              isFullPointsOnly
+            />
+          ),
+          locale: 'en',
+          messages
+        })
+      )
+      expect(ShallowedWrapper.find(FullPointsWrapper)).toHaveLength(1)
+    })
+  })
+
+  describe('Should toggle EarnPoints component', () => {
+    it('renders one <PointsInfo/> if fullPoints is FALSE', () => {
+      const ShallowedWrapper = mount(
+        intl({
+          component: (
+            <ToggleEarnPoints
+              entity={fromJS({
+                ...mockData,
+                points: {
+                  method: {
+                    cod: '0.30',
+                    cash: '0.30',
+                    poc: '0.30'
+                  },
+                  multiplier: '1.50'
+                }
+              })}
+              isFullPointsOnly={false}
+            />
+          ),
+          locale: 'en',
+          messages
+        })
+      )
+      expect(ShallowedWrapper.find(PointsInfo)).toHaveLength(1)
+    })
+    it('renders none <PointsInfo/> if fullPoints is TRUE', () => {
+      const ShallowedWrapper = mount(
+        intl({
+          component: (
+            <ToggleFullPoints
+              entity={fromJS({
+                ...mockData,
+                points: {
+                  method: {
+                    cod: '0.30',
+                    cash: '0.30',
+                    poc: '0.30'
+                  },
+                  multiplier: '1.50'
+                }
+              })}
+              isFullPointsOnly
+            />
+          ),
+          locale: 'en',
+          messages
+        })
+      )
+      expect(ShallowedWrapper.find(PointsInfo)).toHaveLength(0)
+    })
   })
 
   it('renders one <SocialContainer/> styled component', () => {
