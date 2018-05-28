6b5b6f184edb65d8695ecf5a96df58f974e76f8a
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-05-28 17:02:34 +0800

    Use Loader as indication that we cannot process request if items are still processing

--- app/components/Desktop/CategorySection/index.js
+++ app/components/Desktop/CategorySection/index.js
@@ -81,6 +81,8 @@ function CategorySection ({
             <Grid.Column width={3}>
               <div className='padding__top--20'>
                 <FilterSection
+                  itemsLoading={loader}
+
                   queryCategory={queryCategory}
                   queryBrands={queryBrands}
                   requestFromFilter={requestFromFilter}
