3e605a3074da60e8d8346e61714817261c9a4c4b
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-05-29 16:05:17 +0800

    Refactor redux handle for purchases, having its own setter and loader for local and api

--- app/containers/Purchases/reducer.js
+++ app/containers/Purchases/reducer.js
@@ -8,24 +8,30 @@ import { fromJS } from 'immutable'
 import {
   GET_API_PURCHASES,
   GET_LOCAL_PURCHASES,
-  SET_PURCHASES
+  SET_LOCAL_PURCHASES,
+  SET_API_PURCHASES
 } from './constants'
 
 const initialState = fromJS({
   purchases: [],
-  loading: false
+  apiRequestLoading: false,
+  localRequestLoading: false
 })
 
 function purchasesReducer (state = initialState, action) {
   switch (action.type) {
     case GET_LOCAL_PURCHASES:
-      return state.set('loading', true)
+      return state.set('localRequestLoading', true)
     case GET_API_PURCHASES:
-      return state.set('loading', true)
-    case SET_PURCHASES:
+      return state.set('apiRequestLoading', true)
+    case SET_LOCAL_PURCHASES:
       return state
           .set('purchases', fromJS(action.payload))
-          .set('loading', false)
+          .set('localRequestLoading', false)
+    case SET_API_PURCHASES:
+      return state
+          .set('purchases', fromJS(action.payload))
+          .set('apiRequestLoading', false)
 
     default:
       return state
