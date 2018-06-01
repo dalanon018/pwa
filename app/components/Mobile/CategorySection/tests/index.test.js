f25934fde721b5b629d4ce7d92d227a97b815b89
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2018-06-01 11:54:49 +0800

    modified categorysection unit test

--- app/components/Mobile/CategorySection/tests/index.test.js
+++ app/components/Mobile/CategorySection/tests/index.test.js
@@ -1,10 +1,52 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react'
+import { shallow } from 'enzyme'
+import { fromJS } from 'immutable'
 
-// import CategorySection from '../index';
+import CategorySection, { ContentWrapper } from '../index'
+
+const wrapper = (Component = CategorySection, props = {}, enzyme = shallow) => enzyme(
+  <Component {...props} />
+)
 
 describe('<CategorySection />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true)
+  const minProps = {
+    filterBrands: fromJS({}),
+    filterBrandsLoading: false,
+    filterCategories: fromJS({}),
+    filterCategoriesLoading: false,
+    filtered: false,
+    getFilterBrands: () => {},
+    getFilterCategories: () => {},
+    lazyload: false,
+    loader: false,
+    queryBrands: fromJS({}),
+    queryCategory: 'test',
+    requestFromFilter: () => {},
+
+    _displayEmptyLoadingIndicator: () => {},
+    _displayFeaturesProduct: () => {},
+    _displayHeaderFeaturesProduct: () => {},
+    _displayNumberProducts: () => {},
+    _displayRecentlyViewedHeader: () => {},
+    _displayRecentlyViewedItems: () => {},
+    _displayRegularItems: () => {},
+    _handlePageTitle: () => {}
+  }
+
+  it('render without exploding', () => {
+    const renderComponent = wrapper(CategorySection, minProps)
+    expect(
+      renderComponent.length
+    ).toEqual(1)
+  })
+
+  it('should render a div', () => {
+    const renderedComponent = wrapper(CategorySection, minProps)
+    expect(renderedComponent.find('div').length).toEqual(1)
+  })
+
+  it('should render ContentWrapper', () => {
+    const renderComponent = shallow(<ContentWrapper />)
+    expect(renderComponent.length).toEqual(1)
   })
 })
