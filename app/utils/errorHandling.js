af2ef347b11898632584c2df0dc181cfb9c950e5
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-06-27 17:46:08 +0800

    Add coupon_invalid CODE

--- app/utils/errorHandling.js
+++ app/utils/errorHandling.js
@@ -4,7 +4,8 @@ import { switchFn } from 'utils/logicHelper'
 export const ERROR_CODES = {
   VERIFICATION_EXPIRES: 'VERIFICATION_EXPIRES',
   EMPTY_QUANTITY: 'EMPTY_QUANTITY',
-  ERROR_SUBMISSION: 'ERROR_SUBMISSION'
+  ERROR_SUBMISSION: 'ERROR_SUBMISSION',
+  COUPON_INVALID: 'COUPON_INVALID'
 }
 
 export const handleErrorMessage = ({ code, errors, defaultError }) => switchFn(errors)(defaultError)(code)
