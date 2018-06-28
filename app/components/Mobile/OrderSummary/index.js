9946d09e13d1e4186a33907e9fa6434c656d5cf2
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-06-28 19:33:58 +0800

    Make usePoints to maxPoints when Max is greater than Use to prevent negative number

--- app/components/Mobile/OrderSummary/index.js
+++ app/components/Mobile/OrderSummary/index.js
@@ -9,6 +9,7 @@ import PropTypes from 'prop-types'
 import { isEmpty } from 'lodash'
 import {
   T,
+  equals,
   allPass,
   always,
   both,
@@ -376,7 +377,20 @@ class OrderSummary extends React.PureComponent { // eslint-disable-line react/pr
     )(_isFullPointsOnly)
   }
 
+  _checkShouldUpdateUsePoints = (props) => {
+    const usePoints = prop('usePoints')
+    const maxPoints = this._computeTotalPointsPrice(props)
+
+    const shouldUpdateUsePoints = when(
+      compose(lt(maxPoints), usePoints),
+      () => props._updateUsePoints(maxPoints)
+    )
+
+    shouldUpdateUsePoints(props)
+  }
+
   componentWillReceiveProps (nextProps) {
+    const { couponApplied } = this.props
     // when not yet initialize
     const initializeStartingUsePoints = when(
       allPass([
@@ -401,9 +415,15 @@ class OrderSummary extends React.PureComponent { // eslint-disable-line react/pr
         props._updateUsePoints(this._computeTotalPointsPrice(props))
       }
     )
+
+    const updateUsePointsOnPromoApplied = when(
+      compose(complement, equals(couponApplied), prop('couponApplied')),
+      this._checkShouldUpdateUsePoints
+    )
     // we will be using this.props since we are having issue with other deps that use the this.props
     initializeStartingUsePoints(this.props)
     shouldSelectFullPoints(nextProps)
+    updateUsePointsOnPromoApplied(nextProps)
   }
 
   render () {
