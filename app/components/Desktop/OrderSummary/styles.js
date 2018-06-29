5dbc6c2d6a86660ab190e020e1227b22e1bb8479
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-06-29 15:22:07 +0800

    Fix width and height for cliqqIcon make the text bigger

--- app/components/Desktop/OrderSummary/styles.js
+++ app/components/Desktop/OrderSummary/styles.js
@@ -292,10 +292,19 @@ const ProductPriceWrapper = styled.div`
 const FullPointsSideBarWrapper = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
 
   & img {
-    max-width: 28px;
+    width: 34px !important;
+    height: 34px !important;
+    margin-right: 5px !important;
   }
+
+  & b {
+    padding: 0 !important;
+    font-size: 34px !important;
+  }
+
 `
 
 export {
